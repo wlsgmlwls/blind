015e8ff0e497dc4ce1ba55335c0418e88362118c
joeungyeol <cjucmk0831@gmail.com>
2022-11-11 10:45:16 +0900

    은결수정

---
+++ src/main/java/com/jsframe/blind/repository/BoardRepository.java
@@ -0,0 +1,12 @@
+package com.jsframe.blind.repository;
+
+import com.jsframe.blind.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BoardRepository extends CrudRepository<Board, Long> {
+    Page<Board> findByBnoGreaterThan(long bNo, Pageable pageable);
+
+    Page<Board> findByBnumGreaterThan(long bNo, java.awt.print.Pageable pb);
+}
