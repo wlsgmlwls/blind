bcee0b34f58cded6db62d4dc9203134b0936d6f3
jirijeong <zarathustraaa@icloud.com>
2022-11-11 11:11:19 +0900

    ???

--- src/main/java/com/jsframe/blind/repository/CommentRepository.java
+++ src/main/java/com/jsframe/blind/repository/CommentRepository.java
@@ -1,5 +1,6 @@
 package com.jsframe.blind.repository;
 
+import com.jsframe.blind.entity.Comment;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CommentRepository extends CrudRepository<Comment, Long>{
