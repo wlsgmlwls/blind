393b568d9a07ece362f96f03ccb0677f70a644b7
jirijeong <zarathustraaa@icloud.com>
2022-11-11 11:04:02 +0900

    detail생성, 댓글기능..어찌저찌작성

--- src/main/java/com/jsframe/blind/controller/CommentController.java
+++ src/main/java/com/jsframe/blind/controller/CommentController.java
@@ -1,9 +1,19 @@
 package com.jsframe.blind.controller;
 
+import com.jsframe.blind.entity.Comment;
 import com.jsframe.blind.service.CommentService;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.persistence.Column;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@Log
 public class CommentController {
 
     ModelAndView mv;
@@ -11,5 +21,11 @@ public class CommentController {
     @Autowired
     private CommentService cServ;
 
+@PostMapping ("cWriteProc")
+    public String  cWriteProc(Comment comment, HttpSession session, RedirectAttributes rttr){
+    log.info("cWriteProc()");
+    String view = cServ.commentSave(comment,session,rttr);
 
+    return view;
+}
 }
--- src/main/java/com/jsframe/blind/service/CommentService.java
+++ src/main/java/com/jsframe/blind/service/CommentService.java
@@ -1,5 +1,6 @@
 package com.jsframe.blind.service;
 
+import com.jsframe.blind.entity.Comment;
 import com.jsframe.blind.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
@@ -9,7 +10,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.unbescape.html.HtmlEscape;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Service
@@ -25,5 +29,65 @@ public class CommentService {
 
     //댓글 작성
 @Transactional
-    public Long inser
+    public String commentSave(Comment comment, HttpSession session, RedirectAttributes rttr) {
+    log.info("commentSave()");
+    String msg = null;
+    String view = null;
+
+    try {
+        cRepo.save(comment);
+        log.info("c_no : " + comment.getCNo());
+
+        view = "redirect:/detail";
+        msg = "댓글이 작성되었습니다.";
+    } catch (Exception e) {
+        e.printStackTrace();
+        view = "redirect:/detail";
+    }
+    rttr.addFlashAttribute("msg", msg);
+    return view;
+}//commentSave end
+
+@Transactional
+public String commentUpdate(Comment comment, HttpSession session, RedirectAttributes rttr){
+    log.info("commentUpdate()");
+    String msg = null;
+    String view = null;
+
+    try {
+        cRepo.save(comment);
+        msg = "수정되었습니다.";
+        view = "redirect:detail/";
+    } catch (Exception e){
+        e.printStackTrace();
+        msg = "다시 시도해주세요.";
+        view = "redirect:/detail";
+    }
+    rttr.addFlashAttribute("msg",msg);
+    return view;
+} //commentUpdate end
+
+@Transactional
+public String commentDelete(Long cNo, HttpSession session, RedirectAttributes rttr){
+    log.info("commentDelete()");
+    String msg = null;
+    String view = null;
+
+    Comment comment = new Comment();
+    comment.setCNo(cNo);
+
+    //realPath??
+    try {
+        cRepo.deleteById(cNo);
+        msg = "삭제되었습니다.";
+        view = "redirect:/detail";
+    } catch (Exception e){
+        e.printStackTrace();
+        msg = "다시 시도해주세요.";
+        view = "redirect:/detail";
+    }
+    rttr.addFlashAttribute("msg",msg);
+    return view;
+}    //commentDelete end
+
 }
